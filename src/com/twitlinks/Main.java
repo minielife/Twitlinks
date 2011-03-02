5cb6eae8122a0ce24243d2f5fa5fc06fd3384195
Raunak Gupta <raunak.gupta@hotmail.com>
2011-03-02 20:19:51 +0000

    now executes both producer and consumer thread at the same time

--- src/com/twitlinks/Main.java
+++ src/com/twitlinks/Main.java
@@ -1,12 +1,8 @@
 package com.twitlinks;
 
-import java.text.DateFormat;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
-import com.twitlinks.parser.Document;
-import com.twitlinks.parser.Location;
+import com.twitlinks.parser.Parser;
 
 public class Main {
 
@@ -15,13 +11,7 @@ public class Main {
 	 * @throws ParseException 
 	 */
 	public static void main(String[] args) throws ParseException {
-		String str = "Wed, 16 Feb 2011 13:57:14 +0000";
-		DateFormat formatter = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z");
-		Date date = (Date)formatter.parse(str);
-		
-		Location loc = new Location(4.24353, 5.353069);
-		
-		Document d = new Document("this is a test tweet", date, loc, "raunak", 593857398, 938573);
-		System.out.println(d);
+		new Parser().start();
+		new Consumer().start();
 	}
 }
