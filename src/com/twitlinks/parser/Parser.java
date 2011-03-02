8a440a453f9ab3f9c242b99cc48942e2f4c3cd91
Raunak Gupta <raunak.gupta@hotmail.com>
2011-03-02 15:25:15 +0000

    The parser now supports queues which can be consumed by other java classes. I am assumming it should no longer than 10 millisecond to tokenize a tweet.

--- src/com/twitlinks/parser/Parser.java
+++ src/com/twitlinks/parser/Parser.java
@@ -16,9 +16,105 @@
  */
 package com.twitlinks.parser;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 /**
+ * @author raunak
  * @version 1.0
  */
-public class Parser {
+public class Parser extends Thread {
 
+	/** Current thread sleep time */
+	private static int WAIT = 1000;
+
+	/** Minimum length a thread can go to sleep for. 1 second */
+	private static final int MINWAIT = 1000; // 1 SECOND
+
+	/** Maximum length a thread can go to sleep for. 15 Minutes */
+	private static final int MAXWAIT = 900000;
+
+	/**
+	 * Holds a list of <code>Document</code> objects in a queue. The crawler can
+	 * pull approximately 120 tweets/second.
+	 */
+	public static BlockingQueue<Document> documents = new ArrayBlockingQueue<Document>(
+			300);
+
+	/**
+	 * A specialized pattern to parse date with the format
+	 * "Wed Feb 16 13:57:26 GMT 2011"
+	 */
+	private static DateFormat formatter = new SimpleDateFormat(
+			"EEE, d MMM yyyy HH:mm:ss Z");
+
+	/**
+	 * Creates a <code>Document</code> object from a given input.
+	 * 
+	 * @param input
+	 *            A <code>String</code> representation of the document.
+	 * @return A <code>Document</code> object.
+	 * 
+	 * @throws ParseException
+	 */
+	private Document createDocument(String input) throws ParseException {
+		String[] tokens = input.split("\t");
+		return new Document(tokens[1], formatter.parse(tokens[2]), tokens[5],
+				tokens[6], tokens[7]);
+	}
+
+	public void run() {
+		String line = null;
+		boolean success = false;
+		BufferedReader bufferedReader = null;
+
+		// TODO: Connect it to crawler
+		try {
+			bufferedReader = new BufferedReader(new FileReader(
+					"dataset_10k.txt"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		while (true) {
+
+			try {
+				while ((line = bufferedReader.readLine()) != null) {
+					try {
+						success = documents.offer(createDocument(line));
+						if (!success) { // Blocking Queue is full. Increase the
+										// waiting time.
+							try {
+								if (Parser.WAIT < Parser.MAXWAIT) {
+									Parser.WAIT = Parser.WAIT * 2;
+								}
+								Thread.sleep(WAIT);
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
+						} else if (Parser.WAIT > Parser.MINWAIT) {
+							Parser.WAIT = Parser.WAIT - Parser.MINWAIT;
+						}
+					} catch (ParseException pe) {
+						pe.printStackTrace();
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			try {
+				bufferedReader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
